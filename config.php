33e8ca138bb072e20ef432e89a2419d0cd04ff35
acbr5 <nurayse47@gmail.com>
2020-05-16 15:25:47 +0300

    api keys

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
 
 return [
-    'api_key' => '$2a$10$UL7Usqkb3s/o8PPz.ZOxxe3JJtOKObSTkaxqdeONfjp4RhKdMDQuS'
+    'api_key' => '$2a$10$EKveuCEHVz5zic.8WJQHjuqmFYREVSj5V1TH7Tkw8vdLuIeTnVb86'
 ];
