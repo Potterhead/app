69166a9a42f4c82c6abbf77dec4c43ee45bfbe9d
Aysenur BURAK <nurayse47@gmail.com>
2020-05-16 15:30:33 +0300

    Update config.php

--- config.php
+++ config.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    'api_key' => '$2a$10$EKveuCEHVz5zic.8WJQHjuqmFYREVSj5V1TH7Tkw8vdLuIeTnVb86'
+    // https://www.potterapi.com/login/ sitesinden kayıt olun ve $ işareti ile başlayan api anahtarını tek tırnakların içerisine yapıştırın:
+    'api_key' => ''
 ];
