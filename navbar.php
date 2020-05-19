28996d2edde7f7bc2ca22cf4bb4cdd594999ee82
SafaElmali <tsafaelmali@gmail.com>
2020-05-19 22:00:59 +0300

    added ternarary operator

--- navbar.php
+++ navbar.php
@@ -10,16 +10,16 @@
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto">
                     <li class="nav-item">
-                        <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/index.php') { echo 'active'; } ?>" href="index.php">Anasayfa</a>
+                        <a class="nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/index.php')? 'active' : ''?>" href="index.php">Anasayfa</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/characters.php') { echo 'active'; } ?>" href="characters.php">Karakterler</a>
+                        <a class="nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/characters.php')? 'active' : ''?>" href="characters.php">Karakterler</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/houses.php') { echo 'active'; } ?>" href="houses.php">Evler</a>
+                        <a class="nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/houses.php')? 'active' : ''?>" href="houses.php">Evler</a>
                     </li>
                     <li class="nav-item">
-                        <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] == '/spells.php') { echo 'active'; } ?>" href="spells.php">Büyüler</a>
+                        <a class="nav-link <?php echo ($_SERVER['REQUEST_URI'] == '/spells.php')? 'active' : ''?>" href="spells.php">Büyüler</a>
                     </li>
                 </ul>
             </div>
