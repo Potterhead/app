c333c59226a06f9a35b74e556f13ddf0e2fecdc7
SafaElmali <tsafaelmali@gmail.com>
2020-05-19 12:20:26 +0300

    added menu logo  alt attribute

--- navbar.php
+++ navbar.php
@@ -2,7 +2,7 @@
     <div class="container">
         <nav class="navbar navbar-expand-lg navbar-light">
             <a class="navbar-brand" href="#">
-                <img src="assets/images/menu-logo.png" style="width:220px">
+                <img src="assets/images/menu-logo.png" style="width:220px" alt="potterhead_logo">
             </a>
             <button class=" navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
