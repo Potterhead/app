ef4bf0907b0de77da9d93f471f2e8eefe5ff6a1a
alprnkeskekoglu <keskekoglualperen@gmail.com>
2020-05-20 23:35:18 +0300

    api key update

--- config.php
+++ config.php
@@ -2,7 +2,7 @@
 
 return [
     // https://www.potterapi.com/login/ sitesinden kayıt olun ve $ işareti ile başlayan api anahtarını tek tırnakların içerisine yapıştırın:
-    'api_key' => '$2a$10$vw/iGNWhJQltWv4.xBDDSutP8AtahvBQKEPxZLCPwSjzm9efMITIu',
+    'api_key' => '$2a$10$UL7Usqkb3s/o8PPz.ZOxxe3JJtOKObSTkaxqdeONfjp4RhKdMDQuS',
 
 
     "validHouses" => [
